569b991cc23c1cafaf0dc5b1d6c6aab874ad598a
Mashuly211 <mashuly211@gmail.com>
2018-11-06 23:53:04 +0300

    Добавила карту и выровняла элементы в мобильной версии

--- js/script.js
+++ js/script.js
@@ -22,10 +22,10 @@ $(document).ready(function(){
 				$('#slider').slick('unslick');
 				slickIsActive = false;
 			}
-			
+
 		}
 	}
-	
+
 	var burgerBtn =$('.j-nav-toggle');
 	var mobileNav =$('.j-mobile-nav');
 	var isShown = false;
@@ -41,7 +41,7 @@ $(document).ready(function(){
 		}
 	});
 
-	$(window).on('resze', function(){
+	$(window).on('resize', function(){
 		if (window.matchMedia("(min-width: 1200px)").matches) {
 			mobileNav.show();
 			isShown = true;
@@ -52,5 +52,43 @@ $(document).ready(function(){
 			console.log('hide');
 		}
 	});
+
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('contacts__maps', {
+            center: [59.94199306418127,30.279423499999996],
+            zoom: 15
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+
+        myPlacemarkWithContent = new ymaps.Placemark([59.94199306418127,30.279423499999996], {
+            hintContent: 'Собственный значок метки с контентом',
+            iconContent: ''
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: 'images/geotag.png',
+            // Размеры метки.
+            iconImageSize: [38, 49],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-19, -12],
+            // Смещение слоя с содержимым относительно слоя с картинкой.
+            iconContentOffset: [15, 15],
+            // Макет содержимого.
+            iconContentLayout: MyIconContentLayout
+        });
+
+    myMap.geoObjects
+        .add(myPlacemarkWithContent);
+});
 
 });
